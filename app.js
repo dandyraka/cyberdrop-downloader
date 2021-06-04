1641d63e834c26ac091b29fba6d34979909a85b4
Fshy <7390413+Fshy@users.noreply.github.com>
2021-06-04 05:32:50 -0400

    Added progress bars, async & sync methods

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const fs = require('fs');
 const axios = require('axios');
 const chalk = require('chalk');
 const cheerio = require('cheerio');
+const Multiprogress = require('multi-progress');
+const axiosRetry = require('axios-retry');
 const http = require('http');
 const https = require('https');
+
+const multi = new Multiprogress(process.stderr);
+axiosRetry(axios, {
+  retries: 10,
+  retryDelay: axiosRetry.exponentialDelay,
+});
 
 const [, , ...args] = process.argv;
 const url = args[0];
@@ -19,46 +27,83 @@ if (!url) {
       const reqArr = _.map($('.image'), (i) => i.attribs.href);
       const illegalChars = RegExp(/[\\/:"*?<>|]/g);
       const title = $('#title').attr('title').replace(illegalChars, '');
-      if (args[1] && args[1] === ('-d' || '--dir')) {
-        if (!fs.existsSync(title)) {
-          fs.mkdirSync(title);
-        }
+      if (!fs.existsSync(title)) {
+        fs.mkdirSync(title);
       }
       console.log(chalk.magentaBright(title));
       console.log(chalk.greenBright('Starting Download..'));
-      let index = 0;
-      function request() {
-        const filetype = reqArr[index].split('.').pop();
-        let filename = reqArr[index].slice(reqArr[index].lastIndexOf('/') + 1, reqArr[index].length);
+
+      async function downloadImage(img, index) {
+        const filetype = img.split('.').pop();
+        let filename = img.slice(img.lastIndexOf('/') + 1, img.length);
         filename = filename.slice(0, filename.lastIndexOf('-'));
-        return axios({
+        const writer = fs.createWriteStream(`${title}/${filename}.${filetype}`);
+
+        const { data, headers } = await axios({
           method: 'GET',
-          url: reqArr[index].replace('cyberdrop.nl', 'cyberdrop.cc'),
+          url: img.replace('cyberdrop.nl', 'cyberdrop.cc'),
           responseType: 'stream',
           timeout: 60000,
           httpAgent: new http.Agent({ keepAlive: true }),
           httpsAgent: new https.Agent({ keepAlive: true }),
-        }).then((resp) => {
-          console.log(`${chalk.cyanBright(`[${(index + 1).toString().padStart(reqArr.length.toString().length, '0')}/${reqArr.length}]`)} ${filename}.${filetype}`);
-          if (args[1] && args[1] === ('-d' || '--dir')) {
-            resp.data.pipe(
-              fs.createWriteStream(`${title}/${filename}.${filetype}`),
-            );
-          } else {
-            resp.data.pipe(
-              fs.createWriteStream(`${filename}.${filetype}`),
-            );
-          }
-          index += 1;
-          if (index >= reqArr.length) {
-            return console.log(chalk.greenBright('Download Completed!'));
-          }
-          return request();
+        });
+
+        const totalLength = headers['content-length'];
+
+        const bars = [];
+        bars[index] = multi.newBar(`${chalk.cyanBright(`[${(index + 1).toString().padStart(reqArr.length.toString().length, '0')}/${reqArr.length}]`)} ${filename}.${filetype} -> [:bar] :percent :etas`, {
+          width: 40,
+          complete: '=',
+          incomplete: ' ',
+          total: Number.parseInt(totalLength, 10),
+        });
+
+        data.on('data', (chunk) => bars[index].tick(chunk.length));
+        data.pipe(writer);
+
+        return new Promise((resolve, reject) => {
+          writer.on('finish', () => {
+            if (data.aborted) {
+              console.log('aborted', img, index);
+              reject();
+            } else {
+              resolve(true);
+            }
+          });
+          writer.on('error', reject);
         });
       }
-      return request();
-    })
-    .catch((error) => {
-      console.log(error);
+
+      // Sequential
+      switch (args[1]) {
+        case '-s':
+        case '--sequential':
+          // eslint-disable-next-line
+          async function start () {
+            for (let i = 0; i < reqArr.length; i++) {
+              const element = reqArr[i];
+              // eslint-disable-next-line no-await-in-loop
+              await downloadImage(element, i);
+            }
+          }
+          start();
+
+          break;
+
+        default:
+          // Async
+          // eslint-disable-next-line no-case-declarations
+          const promises = [];
+          for (let i = 0; i < reqArr.length; i++) { promises.push(downloadImage(reqArr[i], i)); }
+          // promises.reduce((p, fn) => p.then(fn), Promise.resolve());
+          Promise.all(promises)
+            .then(() => {
+              console.log(chalk.greenBright('Download Completed!'));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          break;
+      }
     });
 }
